09d5c00d7a282da0b80b5316f34819c9f41b9d5b
bada <silverzoo030@gmail.com>
2024-03-25 00:13:06 +0900

    Update 숫자의 합.java
    bufferedWriter로 출력

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.java
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.java
@@ -3,14 +3,17 @@ import java.io.*;
 public class Main {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		// BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int num = Integer.parseInt(br.readLine());
-        String str = br.readLine();
-        int result = 0;
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        	int num = Integer.parseInt(br.readLine());
+        	String str = br.readLine();
+        	int result = 0;
         
-        for(int i=0; i<num; i++) {
-            result += (str.charAt(i) - '0');
-        }
-        System.out.print(result);
+	        for(int i=0; i<num; i++) {
+	            result += (str.charAt(i) - '0');
+	        }
+		bw.write(result+"\n");   // "\n" 하지않으면 틀린답이 됨. bw.write(result) bw.newLine() 이어도 오답임. 왜?
+		bw.flush();
+		bw.close();
+		br.close();
 	}
 }
