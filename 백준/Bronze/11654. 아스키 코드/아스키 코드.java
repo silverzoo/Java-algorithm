2e17e34d9195981b156494bbb90eae93674e9c52
bada <silverzoo030@gmail.com>
2024-03-24 23:37:44 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,8 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        int ch = System.in.read();
+        System.out.print(ch);
+    }
+}
