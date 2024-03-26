7dd5750123489df2c5c85cc348c75853039faa7b
bada <silverzoo030@gmail.com>
2024-03-26 20:10:50 +0900

    [Bronze V] Title: 합, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/8393. 합/합.java
+++ 백준/Bronze/8393. 합/합.java
@@ -5,10 +5,12 @@ public class Main {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int num = Integer.parseInt(br.readLine());
         int result = 0;
-        
-        for(int i = 1; i <=num; i++) {
-            result += i;
+           
+        while(num > 0) {
+            result += num;
+            num--;
         }
+        
         System.out.println(result);
         br.close();
 	}
