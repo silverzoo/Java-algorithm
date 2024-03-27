7647fb1aa6da09dc170ab8bc6c75bce96f5ce9f9
bada <silverzoo030@gmail.com>
2024-03-27 12:08:15 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -1,10 +1,6 @@
 class Solution {
     public int solution(int num1, int num2) {
-        int answer = -1;
-        
-        if(num1 == num2) {
-            answer = 1;
-        }
+        int answer = (num1 == num2) ? 1 : -1;
         return answer;
     }
 }
