728093f9a4c4dd7c96b8871309719d19d28b632b
bada <silverzoo030@gmail.com>
2024-03-27 14:00:03 +0900

    Update 분수의 덧셈.java

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.java
@@ -22,3 +22,22 @@ class Solution {
 		return getGcd(b, a%b);
 	}
 }
+
+/* 분자와 분모의 최대공약수를 내림차순 반복문으로 찾는 방식
+class Solution {
+    public int[] solution(int denum1, int num1, int denum2, int num2) {
+        int mother = num1 * num2;
+        int son1 = num1 * denum2;
+        int son2 = num2 * denum1;
+        int totalSon = son1 + son2;
+        for(int i = mother; i >= 1; i--){
+            if(totalSon % i == 0 && mother % i == 0){
+                totalSon /= i;
+                mother /= i;
+            }
+        }
+        int[] answer = {totalSon, mother};
+        return answer;
+    }
+}
+*/
