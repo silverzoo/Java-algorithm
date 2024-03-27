8940c6ca6456bf3bc535130a163a41ac0e05dafe
bada <silverzoo030@gmail.com>
2024-03-27 15:00:44 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.04 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = new int[numbers.length];
+        
+        for(int i = 0; i < numbers.length; i++) {
+            numbers[i] *= 2;
+            answer[i] = numbers[i];
+        }
+        return answer;
+    }
+}
