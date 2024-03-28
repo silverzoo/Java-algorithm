e5a3308fc9913c89470513a36b0b045233abeb79
bada <silverzoo030@gmail.com>
2024-03-28 23:58:41 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.01 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,21 +1,16 @@
 class Solution {
     public int[] solution(int n) {
-        int[] answer = {};
-        int length; //배열의 길이
-        int j = 0; //배열인덱스++를 위한 장치
+        int length = 0; //배열의 길이
+
+        if(n % 2 == 0) length = n/2;
+        else length = n/2 + 1;
+
+        int[] answer = new int[length];
+        int num = 1; //홀수값을 담을 그릇
         
-        if(n % 2 == 0) {
-            length = n/2;
-        } else {
-            length = n/2+1;
-        }
-        answer = new int[length];
-        
-        for(int i = 0; i <= n; i++) {
-            if(i%2 == 1) {
-                answer[j] = i;
-                j++;
-            }
+        for(int i = 0; i < length; i++) {
+            answer[i] = num;
+            num += 2;
         }
         return answer;
     }
