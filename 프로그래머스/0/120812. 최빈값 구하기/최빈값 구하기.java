1760ff93b0254e4f964dbd7c329c5266310862b7
bada <silverzoo030@gmail.com>
2024-03-29 14:45:29 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.06 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -1,23 +1,21 @@
 class Solution {
     public int solution(int[] array) {
-        int mode = 0; //최빈값 저장
-        int[] index = new int[1000]; //제한사항에 맞춘 인덱스 범위
-        int max = Integer.MIN_VALUE; //최대값 저장, 초기값은 정수형의 최소값
+        int[] index = new int[1000+1];
+        int mode = 0;
+        int max = index[0];
         
         for(int i = 0; i < array.length; i++) {
             index[array[i]]++;
         }
         
-        for(int i =0 ; i < index.length; i++) {
-            //최대값과 최빈값
-            if(max < index[i]) {
+        for(int i = 0; i < index.length; i++) {
+            if(index[i] > max) {
                 max = index[i];
                 mode = i;
-            } else if(max == index[i]) {
-                mode = -1; //최빈값이 여러개일 때
+            } else if(index[i] == max) {
+                mode = -1;
             }
         }
-        
         return mode;
     }
 }
