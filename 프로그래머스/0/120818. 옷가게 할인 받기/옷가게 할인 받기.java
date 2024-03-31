7dc852dbc501583073589cfeba481ba65868a34b
bada <silverzoo030@gmail.com>
2024-03-31 17:40:08 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.03 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,11 +1,6 @@
 class Solution {
     public int solution(int price) {
-        int answer = 0;
-        
-        if(price >= 500000) answer = (int)(price*0.8);
-        else if(price >= 300000) answer = (int)(price*0.9);
-        else if(price >= 100000) answer = (int)(price*0.95);
-        else answer = price;
-        return answer;
+
+        return (int) ((price >= 500000) ? price*0.8 : (price >= 300000) ? price*0.9 : (price >= 100000) ? price*0.95 : price);
     }
 }
