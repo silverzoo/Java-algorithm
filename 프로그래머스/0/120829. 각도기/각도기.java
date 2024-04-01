1f1cec4bf7d787334339d90a933b5f30d60677db
bada <silverzoo030@gmail.com>
2024-04-01 21:03:03 +0900

    [level 0] Title: 각도기, Time: 0.07 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.java
+++ 프로그래머스/0/120829. 각도기/각도기.java
@@ -1,8 +1,27 @@
 class Solution {
     public int solution(int angle) {
-        if(0 < angle && angle < 90) return 1;
-        else if(angle == 90) return 2;
-        else if(angle == 180) return 4;
-        else return 3;
+        int answer = 0;
+        String gak = null;
+        
+        if(0 < angle && angle < 90){
+            gak = "예각";
+        };
+        if(angle == 90){
+            gak = "직각";
+        };
+        if(90 < angle && angle < 180){
+            gak = "둔각";
+        };
+        if(angle == 180){
+            gak = "평각";
+        };
+        
+        return switch(gak) {
+            case "예각" -> 1;
+            case "직각" -> 2;
+            case "둔각" -> 3;
+            case "평각" -> 4;
+            default -> 0;
+        };
     }
-}
+} //return 안에 향상된 switch문 써보고 싶어서.
