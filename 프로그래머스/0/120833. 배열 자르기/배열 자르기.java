f8cc888adb74e4ad04799314c32dbc3893e7c68c
bada <silverzoo030@gmail.com>
2024-04-02 01:27:06 +0900

    [level 0] Title: 배열 자르기, Time: 0.01 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -1,11 +1,7 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
-        int[] answer = new int[num2-num1+1];
-        int start = 0;
-        
-        for(int i = num1; i <= num2; i++) {
-            answer[start++] += numbers[i];
-        }
-        return answer;
+        return Arrays.copyOfRange(numbers, num1, num2+1);
     }
-}
+} // Arrays.copyOfRange(원본배열,시작,마지막+1): 특정 배열의 범위만큼 복사하여 새로운 배열을 만드는 메서드.
