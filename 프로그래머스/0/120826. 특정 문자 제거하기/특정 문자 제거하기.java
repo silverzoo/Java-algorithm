7fcc4f12acb7b320f4e56094292ef86c78392a01
bada <silverzoo030@gmail.com>
2024-04-01 09:46:42 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.03 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -1,11 +1,5 @@
 class Solution {
     public String solution(String my_string, String letter) {
-        String answer = "";
-        for(int i = 0; i < my_string.length(); i++) {
-            if(letter.charAt(0) != my_string.charAt(i)) {
-                answer += my_string.charAt(i);
-            }
-        }
-        return answer;
+        return my_string.replace(letter, "");
     }
 }
