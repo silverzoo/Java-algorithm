c5ba0e38b3dc117e129a9041099635e863c441bc
bada <silverzoo030@gmail.com>
2024-04-06 22:40:37 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.02 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -1,14 +1,9 @@
 class Solution {
     public int solution(String my_string) {
-        //자연수로만 구성되어 있는 문자열 초기화
-        String myNum = my_string.replaceAll("[^1-9]","");
-        //숫자배열 선언
-        int[] numArr = new int[myNum.length()];
-        //문자열 -> 정수형 + 자연수들의 합 구하기
         int answer = 0;
-        for(int i = 0; i < numArr.length; i++) {
-            numArr[i] = myNum.charAt(i)-'0';
-            answer += numArr[i];
+        //문자열 -> 문자형 변환후, 아스키코드로 범위 지정
+        for(char c : my_string.toCharArray()) {
+            if('1' <= c && c <= '9') answer += c-'0';
         }
         return answer;
     }
