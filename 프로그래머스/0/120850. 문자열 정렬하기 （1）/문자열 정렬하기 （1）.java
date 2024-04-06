f1afdb378d5c9432d419be127a5f1f46f6801f48
bada <silverzoo030@gmail.com>
2024-04-06 22:14:13 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.58 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -2,15 +2,17 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(String my_string) {
-        String myNum = my_string.replaceAll("[^0-9]","");
-        String[] numArr = myNum.split("");
-        Arrays.sort(numArr);
-        int[] answer = new int[numArr.length];
-        
-        for(int i = 0; i < numArr.length; i++) {
-            answer[i] = Integer.parseInt(numArr[i]);
+        //숫자만 있는 문자열로 초기화
+        String myNum = my_string.replaceAll("[a-z]","");
+        //숫자배열 선언
+        int[] answer = new int[myNum.length()];
+        //숫자배열에 문자열->정수형으로 숫자 대입
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = myNum.charAt(i)-'0';
         }
+        //숫자배열 오름차순 정렬
+        Arrays.sort(answer);
         
         return answer;
     }
-}
+} //같은 알고리즘이지만 더 정리된 순서로 재작성함.
