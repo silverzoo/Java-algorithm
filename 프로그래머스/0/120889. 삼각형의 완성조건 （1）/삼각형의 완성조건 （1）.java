2856b786831d51a1450bfc4654475195e6a51ff1
bada <silverzoo030@gmail.com>
2024-04-07 23:44:07 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.43 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
+++ 프로그래머스/0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -1,16 +1,8 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] sides) {
-        int longest = sides[0];
-        int rest = 0;
-        //가장 긴변 구하기
-        for(int i = 1; i < 3; i++) {
-            if(longest < sides[i]) {
-                rest += longest;
-                longest = sides[i];
-            } else rest += sides[i];
-        }
-        //삼각형의 완성조건 판단하기
-        if(longest < rest) return 1;
-        else return 2;
+        Arrays.sort(sides);
+        return sides[2] < sides[0]+sides[1] ? 1 : 2;
     }
-}
+} //Arrays.sort() 오름차순 정렬로 쉽게 풀 수 있다
