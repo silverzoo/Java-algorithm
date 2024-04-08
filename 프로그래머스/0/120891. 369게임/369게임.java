9b974bdb39daa47bae7dda82fc10ed26d3067a0b
bada <silverzoo030@gmail.com>
2024-04-08 20:30:41 +0900

    [level 0] Title: 369게임, Time: 8.02 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/0/120891. 369게임/369게임.java
+++ 프로그래머스/0/120891. 369게임/369게임.java
@@ -1,13 +1,14 @@
 class Solution {
     public int solution(int order) {
-        String str = Integer.toString(order);
-        String[] strArr = str.split("");
+        String str = order+"";
         int cnt = 0;
         
-        for(int i = 0; i < strArr.length; i++) {
-            if(strArr[i].equals("3") || strArr[i].equals("6") || strArr[i].equals("9")) 
-                cnt++;
+        for(int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if(c == '3' || c == '6' || c == '9') cnt++;
         }
         return cnt;
     }
 }
+//int -> str: Integer.toString(int n)말고 더 쉬운 방법
+//문자배열로 바꿔서 반복문으로 값비교하는 더 쉬운 방법
