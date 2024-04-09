f8247c5e3b88233fb3ee19d05b1c2f9d0bba2dee
bada <silverzoo030@gmail.com>
2024-04-09 23:51:24 +0900

    [level 0] Title: 약수 구하기, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int n) {
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 1; i <= n; i++) {
+            if(n%i == 0) list.add(i);
+        }
+        
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
