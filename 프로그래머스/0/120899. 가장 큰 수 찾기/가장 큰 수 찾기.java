127c870d1e2c73756562341266eb03ccc1ccf3a1
bada <silverzoo030@gmail.com>
2024-04-10 13:34:57 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.02 ms, Memory: 77 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] array) {
+        
+        int max = array[0];
+        int index = 0;
+        
+        for(int i = 1; i < array.length; i++) {
+            if(max < array[i]) {
+                max = array[i];
+                index = i;
+            }
+        }
+        int[] answer = {max, index};
+        return answer;
+    }
+}
