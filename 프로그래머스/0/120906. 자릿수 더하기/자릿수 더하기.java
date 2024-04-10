9b59280fd9b09c4617b4c08d31cef8e02bc64c68
bada <silverzoo030@gmail.com>
2024-04-10 15:19:01 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.01 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -1,10 +1,10 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        String str = String.valueOf(n);
         
-        for(int i = 0; i < str.length(); i++) {
-            answer += str.charAt(i)-'0';
+        while(n>0) {
+            answer += n%10;
+            n/= 10;
         }
         
         return answer;
