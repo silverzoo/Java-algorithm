1d40dcc4a9707002048c0d47472d1467c13ce7ce
bada <silverzoo030@gmail.com>
2024-04-10 13:49:37 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.13 ms, Memory: 85.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120902. 문자열 계산하기/문자열 계산하기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String my_string) {
+        String[] str = my_string.split(" ");
+        int answer = Integer.parseInt(str[0]);
+        
+        for(int i = 1; i < str.length; i += 2) {
+            if(str[i].equals("+")) answer += Integer.parseInt(str[i+1]);
+            else if(str[i].equals("-")) answer -= Integer.parseInt(str[i+1]);
+        }
+
+        return answer;
+    }
+}
