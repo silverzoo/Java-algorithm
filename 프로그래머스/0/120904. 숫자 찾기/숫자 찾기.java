dae23714ff2b151247124c1913a4aad99fb47e91
bada <silverzoo030@gmail.com>
2024-04-11 16:50:43 +0900

    [level 0] Title: 숫자 찾기, Time: 6.55 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -1,10 +1,6 @@
 class Solution {
     public int solution(int num, int k) {
-        String strNum = num + "";
-        String strK = k + "";
         
-        int answer = strNum.indexOf(strK);
-        
-        return (answer > -1) ? answer+1 : answer;
+        return ("_"+num).indexOf(String.valueOf(k));
     }
-}
+} // 인덱스0의 자리에 임의의 문자열을 추가함으로써 String변환과 자리수출력을 한번에 해결할 수 있다.
