32edabea3123e8d89c248f22b1dbf60ccff0c474
bada <silverzoo030@gmail.com>
2024-04-12 17:31:47 +0900

    [level 0] Title: 문자열안에 문자열, Time: 0.03 ms, Memory: 69.7 MB -BaekjoonHub

--- 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
+++ 프로그래머스/0/120908. 문자열안에 문자열/문자열안에 문자열.java
@@ -1,6 +1,22 @@
 class Solution {
     public int solution(String str1, String str2) {
-        
-        return (str1.contains(str2)) ? 1 : 2;
+        int answer = 0;
+
+        boolean result = false;
+        for (int i = 0; i < str1.length(); i++) {
+            char chr = str1.charAt(i);
+
+            if (chr == str2.charAt(0)) {
+                int len = i + str2.length();
+                String str = (len < str1.length() ? str1.substring(i, len) : str1.substring(i));
+
+                if (str.equals(str2)) { result = true; }
+            }
+        }
+
+        if (result) answer = 1; 
+        else answer = 2;
+
+        return answer;
     }
 }
