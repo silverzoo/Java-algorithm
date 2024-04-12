65f383ac9f2fc79510e29e9005cc2e9c4b784a8d
bada <silverzoo030@gmail.com>
2024-04-12 19:02:45 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.55 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.java
@@ -2,10 +2,11 @@ import java.util.Arrays;
 
 class Solution {
     public String solution(String my_string) {
-        String answer = my_string.toLowerCase();
-        char[] arr = answer.toCharArray();
-        Arrays.sort(arr);
-
-        return new String(arr);
+        char[] c = my_string.toLowerCase().toCharArray();
+        Arrays.sort(c);
+        return new String(c);
     }
 }
+//String클래스가 문자배열을 인자로 받아 새 인스턴스를 생성할 수 있는 이유?
+//String타입의 변수는 문자형배열 char[]을 인스턴스 변수로 정의해놓고 입력받을 때 문자형 배열로 저장된다.
+//String클래스가 불변의 성격(immutable)을 띈다는 게 이런 의미.
