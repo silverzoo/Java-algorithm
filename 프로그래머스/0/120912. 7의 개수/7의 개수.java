e4628ed5072acd95578eafec7a59405533965bc7
bada <silverzoo030@gmail.com>
2024-04-13 23:10:29 +0900

    [level 0] Title: 7의 개수, Time: 0.02 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/0/120912. 7의 개수/7의 개수.java
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -1,12 +1,13 @@
 class Solution {
     public int solution(int[] array) {
         int answer = 0;
-        for(int i = 0; i < array.length; i++) {
-            String str = array[i]+"";
-            for(int j = 0; j < str.length(); j++) {
-                if('7' == str.charAt(j)) answer++;
+        for(int a : array) {
+            while(a > 0) {
+                if(a%10 == 7) answer++;
+                a /= 10;
             }
         }
         return answer;
     }
 }
+//원래 하고 싶었던 로직. 1.향상된 for문 2.배열 요소가 int형일 때 자리수별로 비교
