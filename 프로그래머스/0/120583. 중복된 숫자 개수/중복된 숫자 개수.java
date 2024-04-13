d2e6290660d8f40390229a8fc7b075580ca0dac1
bada <silverzoo030@gmail.com>
2024-04-13 23:42:57 +0900

    [level 0] Title: 중복된 숫자 개수, Time: 0.01 ms, Memory: 66.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120583. 중복된 숫자 개수/중복된 숫자 개수.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] array, int n) {
+        int answer = 0;
+        for(int a : array) {
+            if(a == n) answer++;
+        }
+        return answer;
+    }
+}
