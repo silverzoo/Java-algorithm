bf6eecc8e86f48778c3f9ab939f269068076ffcf
bada <silverzoo030@gmail.com>
2024-04-14 22:58:34 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.34 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.java
@@ -1,13 +1,13 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] numbers) {
-        int max = numbers[0] * numbers[1];
-        for(int i = 0; i < numbers.length; i++) {
-            int factor1 = numbers[i];
-            for(int j = i+1; j < numbers.length; j++) {
-                int factor2 = numbers[j];
-                if(max < factor1 * factor2) max = factor1 * factor2;
-            }
-        }
-        return max;
+        Arrays.sort(numbers);
+        
+        int potential1 = numbers[0] * numbers[1]; //음수끼리 곱해서 최대값이 나올 가능성변수.
+        int potential2 = numbers[numbers.length-1] * numbers[numbers.length-2];
+        return Math.max(potential1, potential2);
     }
 }
+//오름차순 정렬를 활용해서 간단한 로직을 구현하고 싶었는데 음수끼리의 곱을 처리할 아이디어가 떠오르지 않았다.
+//가장 작은 요소 둘의 곱과 가장 큰 요소 둘의 곱을 비교만 하면 된다.
