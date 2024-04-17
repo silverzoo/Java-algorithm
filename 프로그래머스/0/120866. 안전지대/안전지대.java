f760c5f360f4dc10644523be3d8e732a216573ef
bada <silverzoo030@gmail.com>
2024-04-17 22:49:14 +0900

    [level 0] Title: 안전지대, Time: 0.11 ms, Memory: 64.6 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.java
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -1,39 +1,40 @@
 class Solution {
     public int solution(int[][] board) {
         int answer = 0;
-        String[][] arr = new String[board.length][board.length];
-        //새 2차배열 arr에 지뢰좌표와 일치한 인덱스의 요소에 x를 대입한다.
+        
+        //지뢰가 있는 셀 찾기
         for(int i = 0; i < board.length; i++) {
             for(int j = 0; j < board.length; j++) {
                 if(board[i][j] == 1) {
-                    arr[i][j] = "x";
-                } else arr[i][j] = "o";
-            }
-        }
-        //요소값이 x인 arr배열의 인덱스를 반환하여 board배열에 대입, 위험지역을 1로 바꿔준다.
-        for(int i = 0; i < arr.length; i++) {
-            for(int j = 0; j < arr.length; j++) {
-                if(arr[i][j].equals("x")) {
-                    //가장자리에 위치한 지뢰의 경우, IndexOutOfBoundsException 예외처리 필요.
-                    if(i > 0) board[i-1][j] = 1;
-                    if(i < arr.length-1) board[i+1][j] = 1;
-                    if(j > 0) board[i][j-1] = 1;
-                    if(j < arr.length-1) board[i][j+1] = 1;
-                    if(i > 0 && j > 0) board[i-1][j-1] = 1;
-                    if(i < arr.length-1 && j > 0) board[i+1][j-1] = 1;
-                    if(i > 0 && j < arr.length-1) board[i-1][j+1] = 1;
-                    if(i < arr.length-1 && j < arr.length-1) board[i+1][j+1] = 1;
+                    //위험지역으로 분류하기
+                    changeDangerCell(board, i, j);
                 }
             }
         }
-        //최종적으로 안전지역(0인 지역)의 수를 셈한다.
+        
+        //안전지역인 셀 수 반환하기
         for(int i = 0; i < board.length; i++) {
             for(int j = 0; j < board.length; j++) {
-                if(!(board[i][j] == 1)) answer++;
+                if(board[i][j] == 0) answer++;
             }
         }
+        
         return answer;
     }
+    
+    //지뢰에 인접한 8개의 셀을 위험지역으로 분류하는 메서드
+    public void changeDangerCell(int[][] board, int i, int j) {
+        int length = board.length;
+        //인접한 8개의 셀의 좌표값을 가진 배열
+        int[][] directions = {{0,-1},{0,1},{-1,-1},{-1,0},{-1,1},{1,-1},{1,0},{1,1}};
+        
+        for(int[] dir : directions) {
+            int x = i + dir[0];
+            int y = j + dir[1];
+            //보드의 가장자리이거나 이미 지뢰가 매설된 지역이 아니면 위험지역(-1)로 분류하기
+            if(x >= 0 && y >= 0 && x < length && y < length && board[x][y] == 0) {
+                board[x][y] = -1;
+            }
+        }
+    }
 }
-//IndexOutOfBoundsException 예외처리에서 고전했던 문제.
-//객체지향적 접근: 지뢰가 있는 위험지역 처리를 위한 메서드로 리팩토링 해보기.
