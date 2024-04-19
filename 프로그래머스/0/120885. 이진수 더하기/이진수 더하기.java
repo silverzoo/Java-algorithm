5927736cd4ace7a4de4afabd9d5c54bd719a87bb
bada <silverzoo030@gmail.com>
2024-04-19 16:15:39 +0900

    [level 0] Title: 이진수 더하기, Time: 0.05 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String bin1, String bin2) {
+        int num1 = Integer.parseInt(bin1,2);
+        int num2 = Integer.parseInt(bin2,2);
+        return Integer.toBinaryString(num1 + num2);
+    }
+}
+//Integer.parseInt(바꾸려는 문자열,어떤진수)
+//ex) Integer.parseInt("10",2) -> 십진수로 2인 숫자. 문자열"10"을 2진수로 인식하고 변환한다
+//ex) Integer.parseInt("10",10) -> 십진수로 10인 숫자. 문자열"10"을 10진수로 인식하고 변환한다
