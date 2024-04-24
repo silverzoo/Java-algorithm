c4d8d50b4ef605fd2681debbc4de82aa57fb901f
bada <silverzoo030@gmail.com>
2024-04-24 23:18:54 +0900

    [level 1] Title: 특정 옵션이 포함된 자동차 리스트 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/157343. 특정 옵션이 포함된 자동차 리스트 구하기/특정 옵션이 포함된 자동차 리스트 구하기.sql
+++ 프로그래머스/1/157343. 특정 옵션이 포함된 자동차 리스트 구하기/특정 옵션이 포함된 자동차 리스트 구하기.sql
@@ -3,3 +3,19 @@ SELECT CAR_ID, CAR_TYPE, DAILY_FEE, OPTIONS
 FROM CAR_RENTAL_COMPANY_CAR
 WHERE OPTIONS LIKE '%네비게이션%'
 ORDER BY CAR_ID DESC
+
+-- 다른 방식 정리:
+
+-- 1. 정규식 REGEXP
+
+-- SELECT car_id, car_type, daily_fee, options
+-- FROM car_rental_company_car
+-- WHERE options REGEXP '네비게이션'
+-- ORDER BY car_id DESC;
+
+-- 2. INSTR함수 INSTR(문자열 검색위치, 검색하려는 문자열)
+
+-- SELECT car_id, car_type, daily_fee, options
+-- FROM car_rental_company_car
+-- WHERE instr(options, '네비게이션')
+-- ORDER BY car_id DESC;
