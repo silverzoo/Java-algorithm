afa58f4def2f995c18a56c52596a764ec9c0eb89
bada <silverzoo030@gmail.com>
2024-05-02 20:36:02 +0900

    [level 2] Title: 카테고리 별 상품 개수 구하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/131529. 카테고리 별 상품 개수 구하기/카테고리 별 상품 개수 구하기.sql
+++ 프로그래머스/2/131529. 카테고리 별 상품 개수 구하기/카테고리 별 상품 개수 구하기.sql
@@ -2,3 +2,7 @@
 SELECT LEFT(PRODUCT_CODE, 2) AS CATEGORY, COUNT(*) AS PRODUCTS
 FROM PRODUCT
 GROUP BY CATEGORY
+
+# select절에서 사용된 별칭으로 GROUP BY CATEGORY 할때와,
+# GROUP BY PRODUCT_CODE 할때 결과가 다르다.
+# 즉, 별칭이 아닌 원래의 컬럼명으로 그룹화하면 카데고리별로 묶인 개수가 나오지 않는다.(틀린답)
