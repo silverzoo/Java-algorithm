a94d522589aeba231ec9d2b4a33ce307141ee2ea
bada <silverzoo030@gmail.com>
2024-05-21 23:37:03 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 12.16 ms, Memory: 79.7 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,11 +1,8 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        String answer = my_string.substring(0, s) + overwrite_string;
-        
-        if(my_string.length() > answer.length()) {
-            answer += my_string.substring(answer.length());
-        }
+        String before = my_string.substring(0, s);
+        String after = my_string.substring(s + overwrite_string.length());
 
-        return answer;
+        return before + overwrite_string + after;
     }
 }
