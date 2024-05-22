7e0f76d017107829f3b12c1d8f1ad68238db08f4
bada <silverzoo030@gmail.com>
2024-05-22 23:54:27 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.05 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.java
@@ -2,15 +2,11 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         
-        if(n%2 != 0) {
-            for(int i = 1; i <= n; i += 2) {
-                answer += i;
-            }
-        } else {
-            for(int i = 2; i <= n; i += 2) {
-                answer += (i*i);
-            }
+        for(int i = n; i >= 0; i -= 2) {
+            answer += (n%2 == 0) ? (i*i) : i;
         }
         return answer;
     }
 }
+
+//  감소식 반복문은 n으로 시작하므로 시작점이 홀수/짝수인지 판단가능. 더 간단하게 수식을 짤 수 있다.
