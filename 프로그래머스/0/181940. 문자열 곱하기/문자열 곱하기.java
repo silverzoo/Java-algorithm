a45783c9fbe0edf6974852737a14f6b10465a99e
bada <silverzoo030@gmail.com>
2024-05-24 20:11:44 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.03 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.java
@@ -1,9 +1,5 @@
 class Solution {
     public String solution(String my_string, int k) {
-        String answer = "";
-        for(int i = 1; i <= k; i++) {
-            answer += my_string;
-        }
-        return answer;
+        return my_string.repeat(k);
     }
 }
