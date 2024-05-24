19e0d460eee2f3bd98605028c6775fdc0b431330
bada <silverzoo030@gmail.com>
2024-05-24 20:24:43 +0900

    [level 0] Title: 문자열 섞기, Time: 9.27 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,13 +1,14 @@
 class Solution {
     public String solution(String str1, String str2) {
         String answer = "";
-        char[] arr1 = str1.toCharArray();
-        char[] arr2 = str2.toCharArray();
         
-        for(int i = 0; i < arr1.length; i++) {
-            answer += arr1[i];
-            answer += arr2[i];
+        for(int i = 0; i < str1.length(); i++) {
+            answer += str1.charAt(i);
+            answer += str2.charAt(i);
+            //answer += str1.charAt(i) + str2.charAt(i); 은 정수형계산으로 된다.
+            //대신, answer += "" + str1.charAt(i) + str2.charAt(i); 로 문자열계산을 먼저 하게 해주면 해결됨.
         }
+        
         return answer;
     }
 }
