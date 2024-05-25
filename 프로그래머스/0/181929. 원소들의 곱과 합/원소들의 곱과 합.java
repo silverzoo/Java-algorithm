d0e443c6ef5155176421fe8260955f717beb7215
bada <silverzoo030@gmail.com>
2024-05-26 00:22:38 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.03 ms, Memory: 84.3 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -2,14 +2,13 @@ class Solution {
     public int solution(int[] num_list) {
         int mul = 1;
         int sum = 0;
-        int square = 1;
         
         for(int num : num_list) {
             mul *= num;
             sum += num;
         }
-        square = sum*sum;
         
-        return (mul < square)? 1: 0;
+        //제곱함수 사용하기
+        return (mul < Math.pow(sum,2))? 1: 0;
     }
 }
