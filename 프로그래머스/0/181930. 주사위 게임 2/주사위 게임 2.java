d3fe65486dd4cc903c3df0190f363bf22e9b07a2
bada <silverzoo030@gmail.com>
2024-05-29 00:07:56 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.04 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.java
@@ -1,13 +1,15 @@
 class Solution {
     public int solution(int a, int b, int c) {
-        int answer = 0;
-        if(a == b && b == c) {
-            answer = (int)(Math.pow(a,6) * 27) ;
-        } else if(a != b && b != c && a != c) {
-            answer = a + b + c;
-        } else {
-            answer = (int)(Math.pow(a,2) + Math.pow(b,2) + Math.pow(c,2)) * (a + b + c);
+        int answer = 1;
+        int cnt = 1;
+        
+        if(a == b || a == c || b == c) cnt++;
+        if(a == b && a == c) cnt++;
+        
+        for(int i = 1; i <= cnt; i++) {
+            answer *= (int)(Math.pow(a,i) + Math.pow(b,i) + Math.pow(c,i));
         }
         return answer;
     }
 }
+//문제 조건의 유사한 부분을 캐치하고 효율적인 방법의 코드를 짜보기
