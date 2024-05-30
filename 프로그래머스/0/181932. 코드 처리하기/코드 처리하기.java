a2878875bb657beaa464067450b8f86c98c74897
bada <silverzoo030@gmail.com>
2024-05-30 13:04:57 +0900

    [level 0] Title: 코드 처리하기, Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -4,13 +4,12 @@ class Solution {
         int mode = 0;
         
         for(int i = 0; i < code.length(); i++) {
-            if(mode == 0) {
-                if(i%2 == 0 && code.charAt(i) != '1') ret += code.charAt(i);
-                if(code.charAt(i) == '1') mode = 1;
-            } else {
-                if(i%2 != 0 && code.charAt(i) != '1') ret += code.charAt(i);
-                if(code.charAt(i) == '1') mode = 0;
+            if(code.charAt(i) == '1') {
+                mode = (mode == 0)? 1: 0;
+                continue;
             }
+            
+            if(i%2 == mode) ret += code.charAt(i);
         }
         
         return (ret == "")? "EMPTY" : ret;
