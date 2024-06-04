9540d42f73870f36cb963c10f855a43048568c21
bada <silverzoo030@gmail.com>
2024-06-04 23:59:51 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 3.18 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -1,28 +1,23 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
         int[] answer = new int[queries.length];
-        int cnt = 0;
         
-        for(int[] queiry : queries) {
+        for(int cnt = 0; cnt < queries.length; cnt++) {
+            int[] queiry = queries[cnt];
             int min = Integer.MAX_VALUE;
-            int s = queiry[0];
-            int e = queiry[1];
-            int k = queiry[2];
+            int s = queiry[0], e = queiry[1], k = queiry[2];
             
             for(int i = s; i <= e; i++) {
-                if(arr[i] > k && arr[i] < min) {
-                    min = arr[i];
+                if(arr[i] > k) {
+                    min = Math.min(arr[i], min);
                 }
             }
             
-            if(min == Integer.MAX_VALUE) {
-                min = -1;
-            }
-            
-            answer[cnt] = min;
-            cnt++;
+            answer[cnt] = (min == Integer.MAX_VALUE)? -1 : min;
         }
         
         return answer;
     }
 }
+
+//배열의 인덱스 증가를 위한 코드를 써야한다면 향상된for문보다 일반for문을 써서 인덱스로 활용하는 게 나은 것 같다.
