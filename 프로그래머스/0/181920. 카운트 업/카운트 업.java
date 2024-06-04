20e1b7e090e7a9dbc8a0944bd539fc728e686628
bada <silverzoo030@gmail.com>
2024-06-05 00:08:58 +0900

    [level 0] Title: 카운트 업, Time: 0.01 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/0/181920. 카운트 업/카운트 업.java
+++ 프로그래머스/0/181920. 카운트 업/카운트 업.java
@@ -2,10 +2,9 @@ class Solution {
     public int[] solution(int start_num, int end_num) {
         int length = end_num - start_num + 1;
         int[] answer = new int[length];
-        int idx = 0;
         
-        for(int i = start_num; i <= end_num; i++) {
-            answer[idx++] = i;
+        for(int i = 0; i < length; i++) {
+            answer[i] = start_num++;
         }
         
         return answer;
