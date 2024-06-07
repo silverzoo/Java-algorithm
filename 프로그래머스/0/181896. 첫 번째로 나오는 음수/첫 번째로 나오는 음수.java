fdcf46ec32ac26e62ca1ab9501a591db62339342
bada <silverzoo030@gmail.com>
2024-06-07 16:37:09 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.02 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -3,10 +3,9 @@ class Solution {
         int answer = 0;
         for(int i = 0; i < num_list.length; i++) {
             if(num_list[i] < 0) {
-                answer = i;
-                break;
-            } else answer = -1;
+                return i;
+            }
         }
-        return answer;
+        return -1;
     }
 }
