3c1ac8706d54ef7b9a3889651f1053241a1bef4e
bada <silverzoo030@gmail.com>
2024-06-13 22:29:12 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,9 +1,10 @@
 class Solution {
     public int solution(String my_string, String is_suffix) {
-        int answer = 0;
-        StringBuilder sb = new StringBuilder(my_string);
-        StringBuilder sf = new StringBuilder(is_suffix);
-        answer = sb.reverse().indexOf(sf.reverse().toString());
-        return (answer == 0)? 1 : 0;
+        
+        return (my_string.endsWith(is_suffix))? 1 : 0;
     }
 }
+
+//startsWith(String s,int n) -> 특정 문자열이 접두사인지 확인
+//endsWith(String s,int n) -> 특정 문자열이 접미사인지 확인
+//두번째 매개변수의 유무로 시작인덱스를 지정할 수 있다
