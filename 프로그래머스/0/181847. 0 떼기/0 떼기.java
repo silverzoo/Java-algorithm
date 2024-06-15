ce04854ab2ab050a812ffca26b884c7caab0d8ec
bada <silverzoo030@gmail.com>
2024-06-15 22:00:46 +0900

    [level 0] Title: 0 떼기, Time: 0.03 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.java
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -1,20 +1,6 @@
 class Solution {
     public String solution(String n_str) {
-        StringBuilder sb = new StringBuilder();
-        String[] str = n_str.split("");
-        int idx = 0;
-        
-        for(int i = 0; i < str.length; i++) {
-            if(!str[i].equals("0")) {
-                idx = i;
-                break;
-            }
-        }
-        
-        for(int j = idx; j < str.length; j++) {
-            sb.append(str[j]);
-        }
-        
-        return sb.toString();
+        //return "" + Integer.parseInt(n_str);
+        return String.valueOf(Integer.parseInt(n_str));
     }
 }
