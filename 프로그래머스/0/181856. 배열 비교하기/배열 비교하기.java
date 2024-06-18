1f7363f9339e59ebd6167bb69d611a2c08dd424f
bada <silverzoo030@gmail.com>
2024-06-18 23:21:18 +0900

    [level 0] Title: 배열 비교하기, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -1,19 +1,18 @@
 class Solution {
     public int solution(int[] arr1, int[] arr2) {
-        int answer = 0;
+        int answer = Integer.compare(arr1.length, arr2.length);
         
-        if(arr1.length != arr2.length) {
-            answer = (arr1.length > arr2.length)? 1 : -1;
-            return answer;
-        } else {
+        if(answer == 0) {
             int sum1 = 0, sum2 = 0;
             for(int i = 0; i < arr1.length; i++) {
                 sum1 += arr1[i];
                 sum2 += arr2[i];
             }
-            answer = (sum1 == sum2)? 0 : (sum1 > sum2)? 1 : -1;
+            answer = Integer.compare(sum1, sum2);
         }
         
         return answer;
     }
 }
+// Integer.compare() 사용법 :
+// <,=,> 순으로 True 일경우, -1 ,0 , 1의 결과가 나온다.
