4795a6ef4aeebe1a237335596b32eca4ceddffdc
bada <silverzoo030@gmail.com>
2024-06-19 21:42:47 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.07 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,15 +1,8 @@
-import java.util.ArrayList;
-
 class Solution {
     public String[] solution(String my_string) {
-        ArrayList<String> arr = new ArrayList();
         
-        for(String str : my_string.split(" ")) {
-            if(!str.equals("")) {
-                arr.add(str);
-            }
-        }
-        
-        return arr.toArray(new String[arr.size()]);
+        return my_string.trim().split(" +");
     }
 }
+
+//String클래스의 trim()함수는 문자열 양끝의 공백을 제거해준다. 가운데 공백은 제거해주지 않는다.
