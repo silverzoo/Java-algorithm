e4715704992a97a2d018cd5fef6855cb33e29ed3
bada <silverzoo030@gmail.com>
2024-06-20 17:13:42 +0900

    [level 0] Title: 할 일 목록, Time: 9.91 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -1,19 +1,16 @@
 class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
-        int cnt = 0;
-        int idx = 0;
-        
-        for(boolean i : finished) {
-            cnt += (!i)? 1 : 0;
-        }
-        
-        String[] answer = new String[cnt];
+        String answer = "";
         
         for(int i = 0; i < finished.length; i++) {
-            if(!finished[i])
-                answer[idx++] = todo_list[i];
+            answer = (!finished[i])? answer+todo_list[i]+"," : answer;
         }
         
-        return answer;
+        return answer.split(",");
     }
 }
+
+//우려했던 상황 : ","로 여러개 끝나거나 시작되면 공백이 인식되지 않을까?
+//공백이 아닌 null로 인식되어서 올바르게 출력된다.
+
+//단, 중간에 ,,처럼 여러개가 들어가면 공백이 인식이 된다. 왜?
