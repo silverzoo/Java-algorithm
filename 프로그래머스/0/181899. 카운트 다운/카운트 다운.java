0f219fb68678112f5538717e0a392d9a6074ed66
bada <silverzoo030@gmail.com>
2024-06-26 22:18:16 +0900

    [level 0] Title: 카운트 다운, Time: 0.02 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -1,10 +1,9 @@
 class Solution {
     public int[] solution(int start_num, int end_num) {
         int[] answer = new int[start_num-end_num+1];
-        int idx = 0;
         
-        for(int i = start_num; i >= end_num; i--) {
-            answer[idx++] = i;
+        for(int i = 0; i <= start_num - end_num; i++) {
+            answer[i] = start_num - i;
         }
         
         return answer;
