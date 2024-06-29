9cbd0fe89064be7011bd491ad638d2fa9468a370
bada <silverzoo030@gmail.com>
2024-06-29 17:09:40 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.02 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -2,8 +2,9 @@ class Solution {
     public int solution(int[] arr, int idx) {
         int answer = -1;
         
-        for(int i = 0; i < arr.length; i++) {
-            if(arr[i] == 1 && i+1 > idx) {
+        //문제 오류 : "idx보다 크거나 같은" 이라는 전제여야 함
+        for(int i = idx; i < arr.length; i++) {
+            if(arr[i] == 1) {
                 answer = i;
                 break;
             }
