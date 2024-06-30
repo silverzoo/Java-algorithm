9c83eb341a9924162e8ce07560faf08f6e1081ed
bada <silverzoo030@gmail.com>
2024-06-30 21:41:22 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.02 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.java
@@ -5,11 +5,9 @@ class Solution {
         for(int i = 0; i < arr.length; i++) {
             for(int j = 0; j < arr.length; j++) {
                 if(arr[i][j] != arr[j][i]) {
-                    answer = 0;
-                    break;
+                    return answer = 0;
                 }
             }
-            if(answer == 0) break;
         }
 
         return answer;
