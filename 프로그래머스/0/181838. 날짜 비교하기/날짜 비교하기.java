2384dca69744684d97410ed0fb8f33b824964f1b
bada <silverzoo030@gmail.com>
2024-07-07 20:52:43 +0900

    [level 0] Title: 날짜 비교하기, Time: 1.00 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.java
@@ -1,9 +1,11 @@
-import java.util.Arrays;
+import java.time.LocalDate;
 
 class Solution {
     public int solution(int[] date1, int[] date2) {
-        return Arrays.compare(date1, date2) < 0 ? 1 : 0;
+        LocalDate day1 = LocalDate.of(date1[0], date1[1], date1[2]);
+        LocalDate day2 = LocalDate.of(date2[0], date2[1], date2[2]);
+        
+        return (day1.isBefore(day2)) ? 1 : 0;
     }
 }
-//Arrays.compare(int[] a, int[] b)
-//a < b 이면 양의 정수 반환 a == b 이면 0, a > b 이면 음의 정수 반환
+//LocalDate.of 는 파라미터에 들어오는 인자값을 날짜형식으로 바꿔주는 인스턴스 메서드
