2fc1025bd6f5b99df51c38654747b7fee346ef77
bada <silverzoo030@gmail.com>
2024-07-13 01:20:25 +0900

    file moved

--- Algorithm/elice/WeekendTest1.java
+++ Algorithm/elice/WeekendTest1.java
@@ -2,7 +2,7 @@ package elice;
 
 import java.util.Scanner;
 
-public class Main {
+public class WeekendTest1 {
     public static void main(String args[]) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
