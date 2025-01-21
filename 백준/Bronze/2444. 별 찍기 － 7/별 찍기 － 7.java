42e4414d5e38c78365f4c6d880d517b9bd66564e
bada <silverzoo030@gmail.com>
2025-01-21 22:19:06 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 436 ms, Memory: 24884 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j < n-i; j++) {
+                System.out.print(" ");
+            }
+            for(int j = 0; j < i*2-1; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        
+        for(int i = 1; i <= n-1; i++) {
+            for(int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for(int j = 0; j < ((n - i) * 2 - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
